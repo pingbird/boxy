58e71707fd30ebe1cbffcf15ee826cfccd201e7c
Andre Lipke <andre.lipke@gmail.com>
2020-03-28 07:47:39 -0400

    Revert "Remove gallery"
    This reverts commit 5ebe0e99be5fe0ddb11ffecc3075cca843a0458e.

---
+++ examples/gallery/lib/pages/blog_tile.dart
@@ -0,0 +1,364 @@
+import 'dart:math';
+
+import 'package:boxy/boxy.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:boxy_gallery/main.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+import 'package:tuple/tuple.dart';
+
+class BlogTilePage extends StatefulWidget {
+  createState() => BlogTilePageState();
+}
+
+const loremIpsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco...";
+
+class BlogTilePageState extends State<BlogTilePage> {
+  build(BuildContext context) => Scaffold(
+    appBar: GalleryAppBar(
+      ["Boxy Gallery", "Blog Tile"],
+      source: "https://github.com/PixelToast/flutter-boxy/blob/master/examples/gallery/lib/pages/blog_tile.dart",
+    ),
+    backgroundColor: NiceColors.primary,
+    body: Column(children: [
+      Separator(),
+      Expanded(child: Container(child: ListView(children: [
+        Padding(padding: EdgeInsets.only(top: 64)),
+        Center(child: ConstrainedBox(child: BlogTile(
+          body: BlogDesc(
+            author: "Cicero",
+          ),
+          icons: [
+            Tuple2(MdiIcons.shareVariant, null),
+            Tuple2(MdiIcons.starOutline, MdiIcons.star),
+            Tuple2(MdiIcons.heartOutline, MdiIcons.heart),
+            Tuple2(MdiIcons.chatOutline, null),
+          ],
+        ), constraints: BoxConstraints(maxWidth: 450))),
+        Padding(padding: EdgeInsets.only(top: 64)),
+      ], physics: BouncingScrollPhysics()), color: NiceColors.background)),
+      Separator(),
+    ]),
+  );
+}
+
+class ExpandButton extends StatefulWidget {
+  final bool expanded;
+  final VoidCallback onPressed;
+
+  ExpandButton({
+    @required this.expanded,
+    @required this.onPressed,
+  });
+
+  createState() => _ExpandButtonState();
+}
+
+class _ExpandButtonState extends State<ExpandButton> with SingleTickerProviderStateMixin {
+  AnimationController controller;
+
+  @override
+  initState() {
+    super.initState();
+    controller = AnimationController(vsync: this, value: widget.expanded ? 1 : 0);
+    controller.addListener(() => setState(() {}));
+  }
+
+  @override
+  didUpdateWidget(old) {
+    super.didUpdateWidget(old);
+    controller.animateTo(
+      widget.expanded ? 1 : 0,
+      duration: Duration(milliseconds: 500),
+      curve: Curves.ease,
+    );
+  }
+
+  @override
+  dispose() {
+    super.dispose();
+    controller.dispose();
+  }
+
+  static const size = 42.0;
+
+  build(context) => SizedBox(child: Material(
+    borderRadius: BorderRadius.circular(size / 2),
+    color: Colors.transparent,
+    child: InkWell(child: Center(
+      child: Transform.rotate(child: Icon(
+        Icons.arrow_drop_down,
+        size: 24,
+        color: NiceColors.text,
+      ), angle: pi * controller.value),
+    ),
+      onTap: widget.onPressed,
+      hoverColor: Colors.blueGrey.withOpacity(0.1),
+      focusColor: Colors.blueGrey.withOpacity(0.2),
+      highlightColor: Colors.blueGrey.withOpacity(0.3),
+      splashColor: Colors.blueGrey.shade200.withOpacity(0.3),
+    ),
+  ), width: size, height: size);
+}
+
+class BlogDesc extends StatefulWidget {
+  final String author;
+
+  BlogDesc({
+    @required this.author,
+  });
+
+  createState() => _BlogDescState();
+}
+
+class _BlogDescState extends State<BlogDesc> with TickerProviderStateMixin {
+  bool expandDesc = false;
+  bool expandText = false;
+
+  build(BuildContext context) => Stack(children: [
+    Column(children: [
+      AnimatedContainer(
+        margin: EdgeInsets.only(left: 8, bottom: expandDesc ? 4 : 0),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(4),
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [
+              Colors.blueGrey.shade700.withOpacity(expandDesc ? 1 : 0),
+              Colors.blueGrey.shade800.withOpacity(expandDesc ? 1 : 0),
+            ],
+          ),
+        ),
+        duration: Duration(milliseconds: 1000),
+        curve: Curves.ease,
+        height: expandDesc ? 200 : 42,
+        child: Align(child: Padding(child: Row(children: [
+          Padding(child: Icon(
+            MdiIcons.rssBox,
+            color: NiceColors.text,
+          ), padding: EdgeInsets.only(right: 8)),
+          Text(
+            widget.author,
+            style: TextStyle(
+              color: NiceColors.text,
+              fontSize: 18,
+            ),
+          ),
+          Container(
+            height: 2,
+            width: 4,
+            color: NiceColors.text.withOpacity(0.3),
+            margin: EdgeInsets.symmetric(horizontal: 8),
+          ),
+          Text(
+            "1h",
+            style: TextStyle(
+              color: NiceColors.text,
+              fontSize: 15,
+            ),
+          ),
+          Expanded(child: Container(
+            height: 2,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(colors: [
+                NiceColors.text.withOpacity(0.1),
+                NiceColors.text.withOpacity(0.3),
+                NiceColors.text.withOpacity(0.1),
+              ]),
+            ),
+            margin: EdgeInsets.only(left: 12),
+          )),
+        ]), padding: EdgeInsets.only(
+          right: 42,
+          top: 8,
+          bottom: 8,
+          left: 8,
+        )), alignment: Alignment.topCenter),
+      ),
+
+      AnimatedSize(
+        child: Padding(child: Text(
+          loremIpsum,
+          style: TextStyle(color: NiceColors.text),
+          maxLines: 3,
+        ), padding: EdgeInsets.only(
+          left: 8,
+        )),
+        vsync: this,
+        duration: Duration(milliseconds: 500),
+        curve: Curves.ease,
+        alignment: Alignment.topCenter,
+      ),
+    ]),
+
+    Positioned(child: ExpandButton(
+      onPressed: () => setState(() {
+        expandDesc = !expandDesc;
+      }),
+      expanded: expandDesc,
+    ), top: 0, right: 0),
+  ]);
+}
+
+const kButtonSize = 50.0;
+
+class ShareButton extends StatefulWidget {
+  final IconData icon;
+  final IconData alt;
+
+  ShareButton({
+    @required this.icon,
+    this.alt,
+  });
+
+  createState() => _ShareButtonState();
+}
+
+class _ShareButtonState extends State<ShareButton> with SingleTickerProviderStateMixin {
+  bool showAlt = false;
+
+  static final borderRadius = BorderRadius.circular(16);
+
+
+  build(context) => SizedBox(child: Material(
+    color: Colors.transparent,
+    child: InkWell(child: Container(
+      decoration: BoxDecoration(
+        border: Border.all(
+          width: 3,
+          color: NiceColors.divider.withOpacity(0.5),
+        ),
+        borderRadius: borderRadius,
+      ),
+      child: Align(child: Icon((
+        showAlt && widget.alt != null) ?
+          widget.alt : widget.icon,
+        color: Colors.blueGrey.shade200,
+      )),
+    ), onTap: () => setState(() {
+      showAlt = !showAlt;
+    }),
+      hoverColor: Colors.blueGrey.withOpacity(0.1),
+      focusColor: Colors.blueGrey.withOpacity(0.2),
+      highlightColor: Colors.blueGrey.withOpacity(0.3),
+      splashColor: Colors.blueGrey.shade200.withOpacity(0.3),
+    ),
+    clipBehavior: Clip.antiAlias,
+    borderRadius: borderRadius,
+  ),
+    width: kButtonSize,
+    height: kButtonSize,
+  );
+}
+
+class ShareMoreButton extends StatelessWidget {
+  static final borderRadius = BorderRadius.circular(16);
+
+  build(context) => Material(
+    color: Colors.transparent,
+    child: InkWell(child: AnimatedContainer(
+      decoration: BoxDecoration(
+        border: Border.all(
+          width: 3,
+          color: NiceColors.divider.withOpacity(0.5),
+        ),
+        borderRadius: borderRadius,
+      ),
+      width: kButtonSize,
+      height: 30,
+      duration: Duration(milliseconds: 500),
+      curve: Curves.ease,
+      child: Align(child: Icon(
+        Icons.more_horiz,
+        color: Colors.blueGrey.shade200,
+      )),
+    ),
+      onTap: () {},
+      hoverColor: Colors.blueGrey.withOpacity(0.1),
+      focusColor: Colors.blueGrey.withOpacity(0.2),
+      highlightColor: Colors.blueGrey.withOpacity(0.3),
+      splashColor: Colors.blueGrey.shade200.withOpacity(0.3),
+    ),
+    clipBehavior: Clip.antiAlias,
+    borderRadius: borderRadius,
+  );
+}
+
+
+class BlogTile extends StatelessWidget {
+  final Widget body;
+  final List<Tuple2<IconData, IconData>> icons;
+
+  BlogTile({
+    @required this.body,
+    @required this.icons,
+  });
+
+  build(context) => CustomBoxy(
+    delegate: BlogTileDelegate(
+      numButtons: icons.length,
+      buttonBuilder: (context, i) =>
+        ShareButton(icon: icons[i].item1, alt: icons[i].item2),
+    ),
+    children: [
+      LayoutId(id: #body, child: body),
+      LayoutId(id: #moreButton, child: ShareMoreButton()),
+    ],
+  );
+}
+
+class BlogTileDelegate extends BoxyDelegate {
+  int numButtons;
+  IndexedWidgetBuilder buttonBuilder;
+
+  BlogTileDelegate({
+    @required this.numButtons,
+    @required this.buttonBuilder,
+  });
+
+  @override
+  layout() {
+    var moreButton = getChild(#moreButton);
+    var moreButtonSize = moreButton.layout(constraints.loosen());
+
+    var body = getChild(#body);
+    var bodySize = body.layout(constraints.deflate(EdgeInsets.only(
+      left: moreButtonSize.width,
+    )));
+    body.position(Offset(moreButtonSize.width, 0));
+
+    const margin = 8.0;
+    var buttonConstraints = BoxConstraints.tightFor(width: moreButtonSize.width);
+    var offset = 0.0;
+    var limit = bodySize.height - moreButtonSize.height;
+    var finished = false;
+    for (int i = 0; i < numButtons; i++) {
+      var child = inflate(buttonBuilder(buildContext, i));
+      var size = child.layout(buttonConstraints);
+      child.position(Offset(0, offset));
+      var newOffset = offset + size.height + margin;
+      var last = i == numButtons - 1;
+      if (newOffset > (last ? bodySize.height : limit)) {
+        child.ignore();
+        finished = true;
+        break;
+      } else {
+        offset = newOffset;
+      }
+    }
+
+    if (finished) {
+      moreButton.position(Offset(0, offset));
+    } else {
+      moreButton.ignore();
+    }
+
+    return Size(
+      bodySize.width + moreButtonSize.width,
+      bodySize.height,
+    );
+  }
+
+  shouldRelayout(BlogTileDelegate old) => true;
+}
---
+++ examples/gallery/lib/pages/line_numbers.dart
@@ -0,0 +1,268 @@
+import 'dart:math';
+
+import 'package:boxy/boxy.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:boxy_gallery/main.dart';
+import 'package:tuple/tuple.dart';
+
+class LineNumberPage extends StatefulWidget {
+  createState() => LineNumberPageState();
+}
+
+class LineNumberPageState extends State<LineNumberPage> {
+  static const settingsWidth = 400.0;
+  bool constrainWidth = false;
+  double maxWidth = 400.0;
+  double exponent = 1.0;
+  var numberAlignment = Alignment.topRight;
+
+  Widget buildSettings(Widget child) => LayoutBuilder(builder: (ctx, cns) =>
+    cns.maxWidth < settingsWidth ? child : Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [ConstrainedBox(
+        child: child,
+        constraints: BoxConstraints.tightFor(width: settingsWidth),
+      )],
+    ),
+  );
+
+  Widget buildTitle(String name) => Padding(
+    child: Text(
+      name,
+      style: TextStyle(
+        color: NiceColors.text,
+      ),
+    ),
+    padding: EdgeInsets.only(
+      left: 24,
+      top: 8,
+    ),
+  );
+
+  build(BuildContext context) {
+    Widget view = LineNumberView(
+      lineCount: 15,
+      buildLine: (context, i) => Padding(
+        child: Text((String.fromCharCode(i + "a".codeUnitAt(0))) * (1 + i * 2)),
+        padding: EdgeInsets.all(2),
+      ),
+      buildNumber: (context, i) => Padding(
+        child: Text(
+          "${exponent == 1 ? i + 1 : pow(exponent, i).round()}",
+          style: TextStyle(
+            color: NiceColors.text.withOpacity(0.7),
+          ),
+        ),
+        padding: EdgeInsets.symmetric(vertical: 2, horizontal: 8),
+      ),
+      numberBg: Container(
+        color: NiceColors.primary,
+      ),
+      numberAlignment: numberAlignment,
+    );
+
+    var width = constrainWidth ? maxWidth : MediaQuery.of(context).size.width;
+    view = AnimatedContainer(
+      child: view,
+      constraints: BoxConstraints(
+        maxWidth: width,
+        minWidth: constrainWidth ? width : 0,
+      ),
+      duration: Duration(milliseconds: 250),
+      curve: Curves.ease,
+    );
+
+    return Scaffold(
+      appBar: GalleryAppBar(
+        ["Boxy Gallery", "Line Numbers"],
+        source: "https://github.com/PixelToast/flutter-boxy/blob/master/examples/gallery/lib/pages/line_numbers.dart",
+      ),
+      backgroundColor: NiceColors.primary,
+      body: Column(children: [
+        Separator(),
+        Expanded(child: Container(child: ListView(children: [
+          Padding(padding: EdgeInsets.only(top: 64)),
+          Center(child: DecoratedBox(
+            child: ClipRect(child: view),
+            decoration: BoxDecoration(
+              border: Border.all(color: NiceColors.divider, width: 1),
+            ),
+          )),
+          Padding(padding: EdgeInsets.only(top: 64)),
+        ], physics: BouncingScrollPhysics()), color: NiceColors.background)),
+        Separator(),
+        buildSettings(Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
+          Padding(padding: EdgeInsets.only(top: 8)),
+          buildTitle("Constrain width"),
+          Row(children: [
+            Padding(padding: EdgeInsets.only(right: 4)),
+            Switch(
+              value: constrainWidth,
+              onChanged: (b) => setState(() {
+                constrainWidth = b;
+              }),
+            ),
+            Expanded(child: Slider(
+              label: "${maxWidth}px",
+              value: maxWidth,
+              min: 50,
+              max: 600,
+              onChanged: constrainWidth ? (v) => setState(() {
+                maxWidth = v;
+              }) : null,
+            )),
+          ]),
+          Padding(padding: EdgeInsets.only(top: 8)),
+          buildTitle("Align number"),
+          Row(children: [
+            Expanded(child: Slider(
+              label: "x: ${numberAlignment.x.toStringAsFixed(1)}",
+              value: numberAlignment.x,
+              min: -1,
+              max: 1,
+              divisions: 10,
+              onChanged: (v) => setState(() {
+                numberAlignment = Alignment(v, numberAlignment.y);
+              }),
+            )),
+            Expanded(child: Slider(
+              label: "y: ${numberAlignment.y.toStringAsFixed(1)}",
+              value: numberAlignment.y,
+              min: -1,
+              max: 1,
+              divisions: 10,
+              onChanged: (v) => setState(() {
+                numberAlignment = Alignment(numberAlignment.x, v);
+              }),
+            )),
+          ]),
+          Padding(padding: EdgeInsets.only(top: 8)),
+          buildTitle("Number exponent"),
+          Slider(
+            label: "${exponent.toStringAsFixed(1)}",
+            value: exponent,
+            min: 1,
+            max: 10,
+            divisions: 9,
+            onChanged: (v) => setState(() {
+              exponent = v;
+            }),
+          ),
+        ])),
+        Separator(),
+      ]),
+    );
+  }
+}
+
+class LineNumberView extends StatelessWidget {
+  final int lineCount;
+  final IndexedWidgetBuilder buildNumber;
+  final IndexedWidgetBuilder buildLine;
+  final Widget lineBg;
+  final Widget numberBg;
+  final Alignment numberAlignment;
+  final double lineAlignment;
+
+  LineNumberView({
+    @required this.lineCount,
+    @required this.buildNumber,
+    @required this.buildLine,
+    this.lineBg,
+    this.numberBg,
+    this.numberAlignment = Alignment.topRight,
+    this.lineAlignment = 0.0,
+  });
+
+  build(context) => CustomBoxy(
+    children: [
+      if (numberBg != null) LayoutId(id: #numBg, child: numberBg),
+      if (lineBg != null) LayoutId(id: #lineBg, child: lineBg),
+      for (int i = 0; i < lineCount; i++) ...[
+        LayoutId(id: Tuple2(#num, i), child: buildNumber(context, i)),
+        buildLine(context, i),
+      ],
+    ],
+    delegate: LineNumberDelegate(
+      lineCount: lineCount,
+      numberAlignment: numberAlignment,
+      lineAlignment: lineAlignment,
+    ),
+  );
+}
+
+class LineNumberDelegate extends BoxyDelegate {
+  final int lineCount;
+  final Alignment numberAlignment;
+  final double lineAlignment;
+
+  LineNumberDelegate({
+    @required this.lineCount,
+    @required this.numberAlignment,
+    @required this.lineAlignment,
+  });
+
+  @override
+  layout() {
+    var numWidth = 0.0;
+    var numConstraints = constraints.loosen();
+
+    for (int i = 0; i < lineCount; i++) {
+      var size = getChild(Tuple2(#num, i)).layout(numConstraints);
+      numWidth = max(numWidth, size.width);
+    }
+
+    var offset = 0.0;
+    var lineWidth = 0.0;
+
+    var lineConstraints = BoxConstraints(
+      minWidth: max(0.0, constraints.minWidth - numWidth),
+      maxWidth: max(0.0, constraints.maxWidth - numWidth),
+      minHeight: 0.0,
+      maxHeight: double.infinity,
+    );
+
+    for (int i = 0; i < lineCount; i++) {
+      var lineChild = getChild(i);
+      var numChild = getChild(Tuple2(#num, i));
+      var numHeight = numChild.render.size.height;
+
+      var size = lineChild.layout(lineConstraints.copyWith(
+        minHeight: numHeight,
+      ));
+
+      var height = max(size.height, numHeight);
+      var halfHeightDelta = (height - size.height) / 2.0;
+      var lineOffset = offset + halfHeightDelta + lineAlignment * halfHeightDelta;
+
+      lineChild.position(Offset(numWidth, lineOffset));
+      numChild.position(numberAlignment.inscribe(
+        numChild.render.size,
+        Offset(0, offset) & Size(numWidth, height),
+      ).topLeft);
+
+      offset += height;
+      lineWidth = max(lineWidth, size.width);
+    }
+
+    if (hasChild(#numBg)) {
+      getChild(#numBg).layoutRect(Rect.fromLTWH(0, 0, numWidth, offset));
+    }
+
+    if (hasChild(#lineBg)) {
+      getChild(#lineBg).layoutRect(Rect.fromLTWH(numWidth, 0, lineWidth, offset));
+    }
+
+    return Size(
+      lineWidth + numWidth,
+      offset,
+    );
+  }
+
+  @override
+  shouldRelayout(LineNumberDelegate old) =>
+    old.lineCount != lineCount ||
+    old.lineAlignment != lineAlignment ||
+    old.numberAlignment != numberAlignment;
+}
