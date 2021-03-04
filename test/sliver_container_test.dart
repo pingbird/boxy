350eaaf7d5b463a50cbf7385c8bb71ec1bf1aee6
Andre Lipke <andre.lipke@gmail.com>
2021-03-03 19:48:48 -0500

    Fix SliverContainer reclip

--- test/sliver_container_test.dart
+++ test/sliver_container_test.dart
@@ -5,6 +5,32 @@ import 'package:flutter_test/flutter_test.dart';
 import 'common.dart';
 import 'mock_canvas.dart';
 
+class NotifyClipper extends CustomClipper<Path> {
+  final ValueNotifier<Path> notifier;
+
+  NotifyClipper(this.notifier) : super(reclip: notifier);
+  
+  int listeners = 0;
+
+  @override
+  void addListener(VoidCallback listener) {
+    super.addListener(listener);
+    listeners++;
+  }
+
+  @override
+  void removeListener(VoidCallback listener) {
+    listeners--;
+    super.removeListener(listener);
+  }
+
+  @override
+  Path getClip(Size size) => notifier.value;
+
+  @override
+  bool shouldReclip(NotifyClipper oldClipper) => notifier != oldClipper.notifier;
+}
+
 class TestClipper extends CustomClipper<Path> {
   TestClipper(this.path);
   final Path path;
@@ -129,5 +155,75 @@ void main() {
       ..restore(),
     );
     expect(clipper2.wasCalled, true);
+  });
+
+  testWidgets('Reclips when notifyListeners is called', (tester) async {
+    final notifier = ValueNotifier(
+      Path()
+        ..moveTo(0, 0)
+        ..lineTo(10, 0)
+        ..lineTo(10, 10)
+        ..close()
+    );
+
+    final clipper = NotifyClipper(notifier);
+
+    await tester.pumpWidget(TestFrame(
+      child: CustomScrollView(
+        slivers: [
+          SliverContainer(
+            key: const GlobalObjectKey(#container),
+            sliver: const TestSliverChild(),
+            clipper: clipper,
+            clipBehavior: Clip.antiAlias,
+          ),
+        ],
+      ),
+      constraints: const BoxConstraints(maxWidth: 400, maxHeight: 400),
+    ));
+
+    expect(keySliver(#container).paint, paints
+      ..save()
+      ..clipPath(pathMatcher: isPathThat(includes: [
+        const Offset(0, 0),
+        const Offset(10, 0),
+        const Offset(10, 10),
+      ]))
+      ..save()
+      ..path() // Placeholder
+      ..restore()
+      ..restore(),
+    );
+
+    // Should reclip if clipper is notified.
+
+    clipper.notifier.value = Path()
+      ..moveTo(5, 5)
+      ..lineTo(15, 5)
+      ..lineTo(15, 15)
+      ..close();
+
+    await tester.pumpAndSettle();
+
+    expect(keySliver(#container).paint, paints
+      ..save()
+      ..clipPath(pathMatcher: isPathThat(includes: [
+        const Offset(5, 5),
+        const Offset(15, 5),
+        const Offset(15, 15),
+      ]))
+      ..save()
+      ..path() // Placeholder
+      ..restore()
+      ..restore(),
+    );
+
+    // Should remove listeners when unmounted
+
+    expect(clipper.listeners, equals(1));
+
+    await tester.pumpWidget(const SizedBox());
+
+    expect(clipper.listeners, equals(0));
   });
 }
